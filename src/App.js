0e73f9e46a5d5549dc45b327c4e8a11fd8c40f61
jahids <evanjahid321@gmail.com>
2022-03-31 23:24:25 -0700

    login basic design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Component/Login/Login";
+import Register from "./Component/register/Register";
+import Secret from "./Component/Secret";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/secret" element={<Secret/>}></Route>
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
