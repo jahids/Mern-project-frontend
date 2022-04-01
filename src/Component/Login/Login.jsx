5b715523bce5feb182bf5c6695750b833cffab47
jahids <evanjahid321@gmail.com>
2022-04-01 03:38:27 -0700

    class adding

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -5,53 +5,53 @@ import './style.css'
 const Login = () => {
     return (
        
-	<div class="limiter">
-    <div class="container-login100">
-        <div class="wrap-login100 mb-5">
-            <div class="login100-pic js-tilt" data-tilt>
+	<div className="limiter">
+    <div className="container-login100">
+        <div className="wrap-login100 mb-5">
+            <div className="login100-pic js-tilt" data-tilt>
                 <img src="https://www.ictamilaonline.com/wp-content/uploads/2021/06/138-1388103_user-login-icon-login.png" alt="IMG"/>
             </div>
 
-            <form class="login100-form validate-form mb-5">
-                <span class="login100-form-title">
+            <form className="login100-form validate-form mb-5">
+                <span className="login100-form-title">
                     Member Login
                 </span>
 
-                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                    <input class="input100" type="text" name="email" placeholder="Email"/>
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <input className="input100" type="text" name="email" placeholder="Email"/>
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
+                        <i className="fa fa-envelope" aria-hidden="true"></i>
                     </span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                    <input class="input100" type="password" name="pass" placeholder="Password"/>
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-                        <i class="fa fa-lock" aria-hidden="true"></i>
+                <div className="wrap-input100 validate-input" data-validate = "Password is required">
+                    <input className="input100" type="password" name="pass" placeholder="Password"/>
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
+                        <i className="fa fa-lock" aria-hidden="true"></i>
                     </span>
                 </div>
                 
-                <div class="container-login100-form-btn">
-                    <button class="login100-form-btn">
+                <div className="container-login100-form-btn">
+                    <button className="login100-form-btn">
                         Login
                     </button>
                 </div>
 
-                <div class="text-center p-t-12">
-                    <span class="txt1">
+                <div className="text-center p-t-12">
+                    <span className="txt1">
                         Forgot
                     </span>
-                    <a class="txt2" href="#">
+                    <a className="txt2" href="#">
                         Username / Password?
                     </a>
                 </div>
 
-                <div class="text-center p-t-136">
+                <div className="text-center p-t-136">
                   <Link to={'/register'}>
                         Create your Account
-                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
                     </Link>
                 </div>
             </form>
--- src/Component/register/Register.jsx
+++ src/Component/register/Register.jsx
@@ -3,37 +3,37 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-<div class="limiter">
-    <div class="container-login100">
-        <div class="wrap-login100 mb-5">
-            <div class="login100-pic js-tilt" data-tilt>
+<div className="limiter">
+    <div className="container-login100">
+        <div className="wrap-login100 mb-5">
+            <div className="login100-pic js-tilt" data-tilt>
                 <img src="https://www.ictamilaonline.com/wp-content/uploads/2021/06/138-1388103_user-login-icon-login.png" alt="IMG"/>
             </div>
 
-            <form class="login100-form validate-form mb-5">
-                <span class="login100-form-title">
+            <form className="login100-form validate-form mb-5">
+                <span className="login100-form-title">
                     Member Register
                 </span>
 
-                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                    <input class="input100" type="text" name="email" placeholder="Email"/>
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <input className="input100" type="text" name="email" placeholder="Email"/>
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
+                        <i className="fa fa-envelope" aria-hidden="true"></i>
                     </span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                    <input class="input100" type="password" name="pass" placeholder="Password"/>
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-                        <i class="fa fa-lock" aria-hidden="true"></i>
+                <div className="wrap-input100 validate-input" data-validate = "Password is required">
+                    <input className="input100" type="password" name="pass" placeholder="Password"/>
+                    <span className="focus-input100"></span>
+                    <span className="symbol-input100">
+                        <i className="fa fa-lock" aria-hidden="true"></i>
                     </span>
                 </div>
                 
-                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                <div className="wrap-input100 validate-input" data-validate = "Password is required">
                 {/* <label for="cars">Chose the role</label> */}
-                <select placeholder='Role' id="cars" class="input100">
+                <select placeholder='Role' id="cars" className="input100">
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 </select>
@@ -42,18 +42,18 @@ const Register = () => {
                 </div>
 
 
-                <div class="container-login100-form-btn">
-                    <button class="login100-form-btn">
+                <div className="container-login100-form-btn">
+                    <button className="login100-form-btn">
                         Register
                     </button>
                 </div>
 
                 
 
-                <div class="text-center p-t-136">
+                <div className="text-center p-t-136">
                     <Link to={'/'}>
                         login your Account?
-                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
                         </Link>
                 </div>
             </form>
